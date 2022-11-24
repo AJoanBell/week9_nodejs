c39b3146b6fc3e1345507e6d9241470b27bc1dda
Andrea Bell <andrea.bell28@gmail.com>
2022-11-24 12:10:08 +1100

    gitignore and project readme update

--- index.js
+++ index.js
@@ -1,7 +1,60 @@
 // TODO: Include packages needed for this application
 
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'username',
+            message: 'What is your GitHub username?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is your project\'s name?',  
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please write a short description of your project.',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What kind of license should your project have?',
+            choices: ['MIT','APACHE 2.0','GPL 3.0','BSD 3','None'],
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What command should be run to install dependencies?',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'What command should be run to run tests?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What does the user need to know about using the repo?',
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'What does the user need to know about contributing to the repo?'
+        }
+    ]);
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -10,4 +63,11 @@ function writeToFile(fileName, data) {}
 function init() {}
 
 // Function call to initialize app
+const init = () => {
+    promptUser()
+    .then((data) => fs.writeFileSync('README.md', generateMarkdown(data)))
+    .then(() => console.log('Successfully created README.md!'))
+    .catch((err) => console.error(err));
+}
+
 init();
